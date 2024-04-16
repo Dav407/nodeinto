d31ad532970e6cd676851c2391fd2147564d63dd
etim david <etimdavid407@gmail.com>
2024-04-16 12:43:53 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+
+const fs = require("fs")
+
+// const idris = fs.readFileSync("./files/idris.txt", "utf-8")
+// console.log(idris)
+
+
+// const dav = fs.readFileSync("./files/dav.txt", "utf-8")
+// console.log(dav)
+
+
+// const mama = fs.readFileSync("./files/mama.txt", "utf-8")
+// console.log(mama)
+
+// const Speed = "GOOD SPEED"
+// fs.writeFileSync("./files/speed.txt", Speed)
+// console.log(Speed)
+
+// const Baby = "Baby"
+// fs.writeFileSync("./files/baby.txt", Baby)
+// console.log(Baby)
+
+
+fs.readFile("./files/nonBlocking.txt", "utf-8", (error, data) => {
+    if (error){
+        console.log(error)
+    } else {
+    console.log(data)
+    }
+})
+
+fs.readFile("./files/nonBlockingtwo.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err)
+    } else {
+    console.log(data)
+    }
+})
+fs.readFile("./files/nonBlockingthree.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err)
+    } else {
+    console.log(data)
+    }
+})
+
